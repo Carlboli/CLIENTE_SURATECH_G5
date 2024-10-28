e9567cf5055e1488d9500e8a8ad0edc5dbfc7606
Negrorala40 <londono.9906255@gmail.com>
2024-10-28 12:19:11 -0500

    controladores

--- src/controllers/controladorpaciente.js
+++ src/controllers/controladorpaciente.js
@@ -6,11 +6,41 @@
 //2. Por cada variable debo utilizar DOM para
 //asociarl el html con js
 let nombrePaciente=document.getElementById("nombrepaciente")
-let correoPaciente=document.getElementById("")
-let telefonoPaciente=document.getElementById("")
-let fechaNacimientoPaciente=document.getElementById("")
-let ciudadPaciente=document.getElementById("")
+let correoPaciente=document.getElementById("correopaciente")
+let telefonoPaciente=document.getElementById("telefonopaciente")
+let fechaNacimientoPaciente=document.getElementById("nacimientopaciente")
+let ciudadPaciente=document.getElementById("ciudadpaciente")
 let polizaPaciente=document.getElementById("ingresospaciente")
-let ipsPaciente=document.getElementById("")
-let grupoIngresoPaciente=document.getElementById("")
-let fechaAfiliacionPaciente=document.getElementById("")
+let ipsPaciente=document.getElementById("ipspaciente")
+let grupoIngresoPaciente=document.getElementById("ingresospaciente")
+let fechaAfiliacionPaciente=document.getElementById("afiliacionpaciente")
+//3. crear una variable para asociarla al id del boton
+
+let botonRegistroPaciente = document.getElementById("botonregistropaciente")
+
+//4. Detectamos acciones o eventos en el boton
+
+botonRegistroPaciente.addEventListener("click", function(evento){
+    evento.preventDefault()
+    //5. se crea un json que capture todos los datos del formulario
+    let  datosPaciente = {
+        nombre: nombrePaciente.value,
+        correo: correoPaciente.value,
+        telefono:  telefonoPaciente.value,
+        fechaNacimiento: fechaNacimientoPaciente.value,
+        ciudad: ciudadPaciente.value,
+        poliza:  polizaPaciente.value,
+        ips: ipsPaciente.value,
+        grupoIngreso: grupoIngresoPaciente.value,
+        fechaAfiliacion: fechaAfiliacionPaciente.value
+    }
+    
+    console.log(datosPaciente)
+
+    Swal.fire({
+        title: "Ahora haces parte de sura :D",
+        text: "Registro exitoso!",
+        icon: "success"
+      });
+      
+})  
---
+++ src/controllers/controladorsignovital.js
@@ -0,0 +1,25 @@
+let FechaDeToma = document.getElementById("fechatoma")
+let descripcionSigno = document.getElementById("descripcion")
+let seleccionaSigno = document.getElementById("signovital")
+let botonRegistroSignoVital = document.getElementById("botonregistrosignovital")
+
+
+botonRegistroSignoVital.addEventListener("click",function(evento){
+    evento.addEventListener()
+
+    let datosSignoVital = {
+    fechaDeToma: FechaDeToma.value,
+    descripcion: descripcionSigno.value,
+    signoVital: seleccionaSigno.value
+    }
+    console.log(datosSignoVital)
+
+
+    Swal.fire({
+        title: "Ahora haces parte de sura :D",
+        text: "Registro exitoso!",
+        icon: "success"
+      });
+
+
+})
